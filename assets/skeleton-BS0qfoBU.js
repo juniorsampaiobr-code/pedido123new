e0b8ea1fd34c6cb9b39d015b0d5768dfa6e7c043
juniorsampaiobr-code <juniorsampaiobr-code@users.noreply.github.com>
2025-11-18 00:41:17 +0000

    deploy: b86aef6706ef2f8029e9d048c1545bc8b1b0bfe4

---
+++ assets/skeleton-BS0qfoBU.js
@@ -0,0 +1,1 @@
+import{j as m}from"./tanstack-QxCZ3Wpf.js";import{b as o}from"./index-BKj90VFL.js";function n({className:e,...t}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-muted",e),...t})}export{n as S};
