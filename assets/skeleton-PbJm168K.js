4e818444e31c2377d384a537b747f1858bca913a
juniorsampaiobr-code <juniorsampaiobr-code@users.noreply.github.com>
2025-11-20 18:26:03 +0000

    deploy: 0d37217a10dc5db985198ad85c5596847bee0770

--- assets/skeleton-PbJm168K.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./tanstack-DVJTcRV8.js";import{a as o}from"./index-B4j8sqPs.js";function a({className:e,...t}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-muted",e),...t})}export{a as S};
--- assets/textarea-B6WCDVi1.js
+++
@@ -1,6 +0,0 @@
-import{c as a,a as t}from"./index-B4j8sqPs.js";import{j as i}from"./tanstack-DVJTcRV8.js";import{r as s}from"./vendor-C5yldPn0.js";/**
- * @license lucide-react v0.462.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const c=a("Upload",[["path",{d:"M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4",key:"ih7n3h"}],["polyline",{points:"17 8 12 3 7 8",key:"t8dd8p"}],["line",{x1:"12",x2:"12",y1:"3",y2:"15",key:"widbto"}]]),n=s.forwardRef(({className:e,...o},r)=>i.jsx("textarea",{className:t("flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",e),ref:r,...o}));n.displayName="Textarea";export{n as T,c as U};
