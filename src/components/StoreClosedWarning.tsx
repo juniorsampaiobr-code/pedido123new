d0bf80dee17e3b40e03830be4e1cfd2b0a44082d
[dyad] <juniorsampaiobr@gmail.com>
2025-11-22 15:55:45 -0300

    [dyad] Simplificando a mensagem de aviso de loja fechada para 'A loja está fechada'. - wrote 1 file(s)

--- src/components/StoreClosedWarning.tsx
+++ src/components/StoreClosedWarning.tsx
@@ -11,8 +11,7 @@ const StoreClosedWarningComponent = ({ todayHours }: StoreClosedWarningProps) =>
       <Clock className="h-4 w-4" />
       <AlertTitle>Loja Fechada</AlertTitle>
       <AlertDescription>
-        A loja está fechada no momento. Nosso horário de funcionamento hoje é das {todayHours}.
-        Você pode fazer o pedido, mas ele só será processado quando reabrirmos.
+        A loja está fechada.
       </AlertDescription>
     </Alert>
   );
