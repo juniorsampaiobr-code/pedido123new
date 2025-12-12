4314aab62533ea6ffb6d659bfd6850094b3ff2d7
juniorsampaiobr-code <juniorsampaiobr-code@users.noreply.github.com>
2025-12-12 02:24:17 +0000

    deploy: 8735465398fc567cd43b059ab7f1fc4a8166eda7

---
+++ assets/Logo-k-8hQCFq.js
@@ -0,0 +1,1 @@
+import{j as s}from"./tanstack-d4AvmB45.js";import{a as t}from"./index-C4gbHgS6.js";import{S as e}from"./shopping-cart-CI7libod.js";const i=({className:r})=>s.jsxs("div",{className:t("flex items-center gap-2",r),children:[s.jsx(e,{className:"h-6 w-6 text-primary"}),s.jsx("span",{className:"text-xl font-bold text-foreground",children:"Pedido 123"})]});export{i as L};
